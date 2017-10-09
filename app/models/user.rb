c18304273c07bc40d1a958ace0596b24acaa74d3
Simon Muriuki <symonmuriuki411@gmail.com>
2017-10-09 13:16:38 +0300

    app

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :secure_validatable, :confirmable, :lockable
-after_initialize do
+before_create do
   o = [('1'..'9'), ('A'..'Z')].map(&:to_a).flatten
     self.user_code ||=  (0...8).map { o[rand(o.length)] }.join
 
