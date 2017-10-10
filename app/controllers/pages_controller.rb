5a2f2fa378a36c129a1732f6b2d33d49c18faa10
Simon Muriuki <symonmuriuki411@gmail.com>
2017-10-10 12:13:25 +0300

    restrict

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
- before_action :authenticate_user!  
+ before_action :authenticate_user! 
+ before_action :check_plan
  def welcome
   end
-  
+  private
+def check_plan
+  unless current_user.plan_id
+    redirect_to choose_plan_path
+
+  end
+ end
+ 
 end
