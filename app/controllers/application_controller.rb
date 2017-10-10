c475440f954f1ec9729e7ff15487b32396bbc6a2
Simon Muriuki <symonmuriuki411@gmail.com>
2017-10-10 12:30:08 +0300

    app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,5 @@ class ApplicationController < ActionController::Base
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
 end
-def check_plan
-  if current_user.plan_id.nil?
-    redirect_to choose_plan_path
-  else
-    redirect_to admin_path
-  end
- end
+
 end
