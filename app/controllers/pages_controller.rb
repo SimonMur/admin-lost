b52cd3a191b6fc5d8b9a93024d3d261fdb736336
Simon Muriuki <symonmuriuki411@gmail.com>
2017-10-16 14:57:12 +0300

    app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,5 +12,13 @@ def check_plan
 
   end
  end
- 
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+end
 end
