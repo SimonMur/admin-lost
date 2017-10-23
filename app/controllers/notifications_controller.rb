ceaec6111ff7c5f9255b0ffef431ca910dae8f2d
Simon Muriuki <symonmuriuki411@gmail.com>
2017-10-23 09:10:26 +0300

    app

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   def link_through
   	@notification = Notification.find(params[:id])
   @notification.update read: true
-  redirect_to post_path @notification.post
+  redirect_to notifications_path
   end
   def index
   	 @notifications = current_user.notifications
