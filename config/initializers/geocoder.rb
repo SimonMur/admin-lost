51b97e2eda29f565c20dfd7ca7f4823b8a4e7c29
Simon Muriuki <symonmuriuki411@gmail.com>
2017-10-31 19:36:12 +0300

    app

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,9 +7,9 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
- api_key: "AIzaSyC_NY_QCFrxoDKhpShoofAr7htp4TKxtwg"            # API key for geocoding service
+ api_key: "AIzaSyC_NY_QCFrxoDKhpShoofAr7htp4TKxtwg",            # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+  cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
