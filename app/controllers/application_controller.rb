452d6977ea5efa8742c041a3f3123b77e0b91052
Simon Muriuki <symonmuriuki411@gmail.com>
2017-11-10 17:08:00 +0300

    app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   # reset captcha code after each request for security
   after_action :reset_last_captcha_code!
-
+before_action :load_notification
   # reset captcha code after each request for security
  
-
+	def load_notification
+		if current_user
+		@notifications = current_user.notifications.order('created_at DESC').limit(3)
+	else
+	end
+	end
   protect_from_forgery with: :exception
 
 end
