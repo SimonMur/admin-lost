5ff65daa74887ac18e68da9273876f63a2eb9c54
Simon Muriuki <symonmuriuki411@gmail.com>
2017-11-11 15:40:59 +0300

    app

--- app/models/lostitem.rb
+++ app/models/lostitem.rb
@@ -2,6 +2,8 @@ class Lostitem < ApplicationRecord
   belongs_to :type
   belongs_to :county
   belongs_to :user
+   mount_uploader :picture_front, PictureUploader
+    mount_uploader :picture_back, PictureUploader
  def self.search(search)
   if search
      where('id_number LIKE ?', "%#{search}%")
